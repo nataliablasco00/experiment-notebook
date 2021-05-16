0483be5cdd7e5dbe9bfc98f9ade489525fe9b687
nataliablasco00 <natalia.blascoa@e-campus.uab.cat>
2021-05-16 16:15:00 +0200

    new get_df in ML_Experiment

--- enb/ml.py
+++ enb/ml.py
@@ -16,8 +16,9 @@ class Model(experiment.ExperimentTask):
         param_dict['criterion'] = criterion
         super().__init__(param_dict=param_dict)
 
-    def test(self, test_loader):
-        self.eval()
+    def test(self, test_loader, batch_size_test=1):
+        self.param_dict['model'].eval()
+        test_loader = torch.utils.data.DataLoader(test_loader, batch_size=batch_size_test, shuffle=True)
         test_loss = 0
         correct = 0
         totals = 0
@@ -59,11 +60,9 @@ class MachineLearningExperiment(experiment.Experiment):
             self._training_results = None
             self._testing_results = None
 
-        @property
         def testing_results(self):
             """Perform the actual testing experiment for the selected row.
             """
-            print("HELLO")
             if self._testing_results is None:
                 time_before = time.time()
                 self._testing_results = self.model.test(self.testing_dataset_path)
@@ -142,12 +141,97 @@ class MachineLearningExperiment(experiment.Experiment):
         model = self.models_by_name[model_name]
         image_info_row = self.dataset_table_df.loc[indices_to_internal_loc(file_path)]
         row_wrapper = self.RowWrapper(file_path, model, row)
+        row_wrapper.testing_results
+
         result = super().process_row(index=index, column_fun_tuples=column_fun_tuples,
                                      row=row_wrapper, overwrite=overwrite, fill=fill)
-
 
         if isinstance(result, Exception):
             return result
 
-        print(result.__dict__)
         return row
+
+    def get_df(self, target_indices=None, target_columns=None,
+               fill=True, overwrite=None, parallel_row_processing=None,
+               chunk_size=None):
+        """Get a DataFrame with the results of the experiment. The produced DataFrame
+        contains the columns from the dataset info table (but they are not stored
+        in the experiment's persistence file).
+
+        :param parallel_row_processing: if True, parallel computation is used to fill the df,
+          including compression. If False, sequential execution is applied. If None,
+          not options.sequential is used.
+        :param target_indices: list of file paths to be processed. If None, self.target_file_paths
+          is used instead.
+        :param chunk_size: if not None, a positive integer that determines the number of table
+          rows that are processed before made persistent.
+        :param overwrite: if not None, a flag determining whether existing values should be
+          calculated again. If none, options
+        """
+        print("########################3", target_indices)
+        target_indices = self.target_file_paths if target_indices is None else target_indices
+        print("########################3", target_indices)
+        overwrite = overwrite if overwrite is not None else options.force
+        target_tasks = list(self.tasks)
+        parallel_row_processing = parallel_row_processing if parallel_row_processing is not None \
+            else not options.sequential
+
+        self.tasks_by_name = collections.OrderedDict({task.name: task for task in target_tasks})
+        target_task_names = [t.name for t in target_tasks]
+        target_indices = tuple(itertools.product(
+            sorted(set(target_indices)), sorted(set(target_task_names))))
+
+        target_indices = list(target_indices)
+        assert len(target_indices) > 0, "At least one index must be provided"
+
+        chunk_size = chunk_size if chunk_size is not None else options.chunk_size
+        chunk_size = chunk_size if chunk_size is not None else len(target_indices)
+        chunk_size = chunk_size if not options.quick else len(target_indices)
+        assert chunk_size > 0, f"Invalid chunk size {chunk_size}"
+        chunk_list = [target_indices[i:i + chunk_size] for i in range(0, len(target_indices), chunk_size)]
+        assert len(chunk_list) > 0
+        for i, chunk in enumerate(chunk_list):
+            if options.verbose:
+                print(f"[{self.__class__.__name__}:get_df] Starting chunk {i + 1}/{len(chunk_list)} "
+                      f"@@ {100 * i * chunk_size / len(target_indices):.1f}"
+                      f"-{min(100, 100 * ((i + 1) * chunk_size) / len(target_indices)):.1f}% "
+                      f"({datetime.datetime.now()})")
+            df = self.get_df_one_chunk(
+                target_indices=chunk, target_columns=target_columns, fill=fill,
+                overwrite=overwrite, parallel_row_processing=parallel_row_processing)
+        if len(chunk_list) > 1:
+            # Get the full df if more thank one chunk is requested
+            df = self.get_df_one_chunk(
+                target_indices=target_indices, target_columns=target_columns, fill=fill,
+                overwrite=overwrite, parallel_row_processing=parallel_row_processing)
+        return df
+
+        # Add dataset columns
+        rsuffix = "__redundant__index"
+        df = df.join(self.dataset_table_df.set_index(self.dataset_info_table.index),
+                     on=self.dataset_info_table.index, rsuffix=rsuffix)
+        if options.verbose:
+            redundant_columns = [c.replace(rsuffix, "")
+                                 for c in df.columns if c.endswith(rsuffix)]
+            if redundant_columns:
+                print("[W]arning: redundant dataset/experiment column(s): " +
+                      ', '.join(redundant_columns) + ".")
+
+        # Add columns based on task parameters
+        if len(df) > 0:
+            task_param_names = set()
+            for task in self.tasks_by_name.values():
+                for k in task.param_dict.keys():
+                    task_param_names.add(k)
+            for param_name in task_param_names:
+                def get_param_row(row):
+                    file_path, task_name = row[self.index]
+                    task = self.tasks_by_name[task_name]
+                    try:
+                        return task.param_dict[param_name]
+                    except KeyError as ex:
+                        return None
+
+                df[param_name] = df.apply(get_param_row, axis=1)
+
+        return df[(c for c in df.columns if not c.endswith(rsuffix))]
--- templates/ML_experiment/ml_experiment.py
+++ templates/ML_experiment/ml_experiment.py
@@ -2,14 +2,42 @@ from enb.config import get_options
 
 from enb import ml
 import model_Resnet18.model_resnet18
+import torch
+import torchvision
 
 options = get_options(from_main=False)
 
 
 if __name__ == '__main__':
 
-
     models = []
+
+    n_epochs = 3
+    batch_size_train = 64
+    batch_size_test = 1000
+    learning_rate = 0.01
+    momentum = 0.5
+    log_interval = 10
+
+    random_seed = 1
+    torch.backends.cudnn.enabled = False
+    torch.manual_seed(random_seed)
+
+    torchvision.datasets.MNIST('./data/', train=True, download=True,
+                                   transform=torchvision.transforms.Compose([
+                                       torchvision.transforms.ToTensor(),
+                                       torchvision.transforms.Normalize(
+                                           (0.1307,), (0.3081,))
+                                   ]))
+
+    torchvision.datasets.MNIST('./data/', train=False, download=True,
+                                   transform=torchvision.transforms.Compose([
+                                       torchvision.transforms.ToTensor(),
+                                       torchvision.transforms.Normalize(
+                                           (0.1307,), (0.3081,))
+                                   ]))
+    options.base_dataset_dir = "./data/MNIST/processed"
+
     models.append(model_Resnet18.model_resnet18.Resnet18(2))
 
     exp = ml.MachineLearningExperiment(models=models)
